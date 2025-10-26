4d57abe1966c620ac147681744a806dcabf9bd11
Kavya <146416207+Kavya-sm@users.noreply.github.com>
2025-10-26 23:53:53 +0530

    aws-exports.js

---
+++ src/frontend/aws-exports.js
@@ -0,0 +1,13 @@
+const awsmobile = {
+  "aws_project_region": "us-east-1",
+  "aws_cognito_identity_pool_id": "us-east-1:00000000-0000-0000-0000-000000000000",
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_000000000",
+  "aws_user_pools_web_client_id": "000000000000000000000000000",
+  "oauth": {},
+  "aws_appsync_graphqlEndpoint": "https://example.com/graphql",
+  "aws_appsync_region": "us-east-1",
+  "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
+};
+
+export default awsmobile;
