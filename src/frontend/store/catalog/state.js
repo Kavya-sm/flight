128b2cdc1f63f81265d3e4f2d305001fb5859b5c
Kavya <146416207+Kavya-sm@users.noreply.github.com>
2025-11-11 16:52:21 +0530

    state.js

--- src/frontend/store/catalog/state.js
+++ src/frontend/store/catalog/state.js
@@ -1,12 +1,9 @@
 /**
- * Catalog [Vuex Module](https://vuex.vuejs.org/guide/modules.html) - It holds flights information from Catalog service.
- * @see {@link SET_FLIGHTS} for more info on mutation
- * @see {@link SET_LOADER} for more info on mutation
- * @see {@link fetchFlights} for more info on amodule ction
- * @see {@link fetchByFlightNumber} for more info on module action
+ * Catalog Vuex Module State
+ * Holds flight data, loader status, and pagination token.
  */
 export default {
   flights: [],
   loading: false,
-  paginationToken: ""
+  paginationToken: null
 };
