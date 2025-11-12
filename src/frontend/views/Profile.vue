96e9afa1cd3c6732c695240ad1165960811e2b77
Kavya <146416207+Kavya-sm@users.noreply.github.com>
2025-11-12 22:51:06 +0530

    Update Profile.vue

--- src/frontend/views/Profile.vue
+++ src/frontend/views/Profile.vue
@@ -13,7 +13,7 @@
             class="q-title loyalty__heading-tier-name"
             data-test="loyalty-level"
           >
-            {{ loyalty.level || "bronze" }}
+            {{ formattedLevel }}
           </div>
         </div>
       </div>
@@ -39,29 +39,35 @@
         <div class="row">
           <div class="profile__preferences--heading col-12 text-left q-mt-lg">
             <q-toolbar color="grey-1 text-black">
-              <q-toolbar-title class="text-bold">Preferences</q-toolbar-title>
+              <q-toolbar-title class="text-bold">Loyalty Information</q-toolbar-title>
             </q-toolbar>
           </div>
           <div class="profile__preferences-options col-12">
-            <q-list highlight no-border class="q-pa-none q-ml-md" link>
-              <a @click="choosePreference('diet')">
-                <q-item class="q-pa-none q-mt-md profile__preferences-option">
-                  <q-icon name="tune" size="2.6rem" />
-                  <q-item-main
-                    class="text-bold q-title q-ml-md"
-                    label="Dietary requirements"
-                  />
-                </q-item>
-              </a>
-              <a @click="choosePreference('luggage')">
-                <q-item class="q-pa-none q-mt-md profile__preferences-option">
-                  <q-icon name="tune" size="2.6rem" />
-                  <q-item-main
-                    class="text-bold q-title q-ml-md"
-                    label="Luggage"
-                  />
-                </q-item>
-              </a>
+            <q-list highlight no-border class="q-pa-none q-ml-md">
+              <q-item class="q-pa-none q-mt-md">
+                <q-icon name="star" size="2.6rem" color="secondary" />
+                <q-item-main
+                  class="text-bold q-title q-ml-md"
+                  label="Current Tier"
+                  :sublabel="formattedLevel"
+                />
+              </q-item>
+              <q-item class="q-pa-none q-mt-md">
+                <q-icon name="card_giftcard" size="2.6rem" color="primary" />
+                <q-item-main
+                  class="text-bold q-title q-ml-md"
+                  label="Points to Next Tier"
+                  :sublabel="loyalty.remainingPoints || 0"
+                />
+              </q-item>
+              <q-item class="q-pa-none q-mt-md">
+                <q-icon name="trending_up" size="2.6rem" color="positive" />
+                <q-item-main
+                  class="text-bold q-title q-ml-md"
+                  label="Progress"
+                  :sublabel="`${loyalty.percentage || 0}% to next tier`"
+                />
+              </q-item>
             </q-list>
           </div>
         </div>
@@ -70,160 +76,3 @@
     </div>
   </div>
 </template>
-
-<script>
-// @ts-nocheck
-import { mapState, mapGetters } from "vuex";
-import { AmplifyEventBus } from "aws-amplify-vue";
-
-/**
- *
- * Profile view displays a current Loyalty progress and points, and allow customers to set preferences.
- */
-export default {
-  name: "Profile",
-  /**
-   * @param {object} user - Current authenticated user from Profile module
-   * @param {boolean} isAuthenticated - Getter from Profile module
-   * @param {Loyalty} loyalty - Loyalty data from Loyalty module
-   */
-  computed: {
-    ...mapState({
-      user: state => state.profile.user,
-      loyalty: state => state.loyalty.loyalty
-    }),
-    ...mapGetters({
-      isAuthenticated: "profile/isAuthenticated",
-      firstName: "profile/firstName",
-      lastName: "profile/lastName"
-    }),
-    fullName() {
-      return `${this.firstName} ${this.lastName}`;
-    }
-  },
-  methods: {
-    choosePreference(option) {
-      const defaultDialogOpts = {
-        cancel: true,
-        preventClose: true,
-        color: "secondary"
-      };
-
-      const luggageDialog = {
-        title: "Luggage preference",
-        message: "How many luggages would you like to check-in with?",
-        options: {
-          type: "radio",
-          model: "opt2",
-          items: [
-            { label: "1", value: "1", color: "primary" },
-            { label: "2", value: "2" },
-            { label: "3", value: "3" }
-          ]
-        }
-      };
-
-      const dietaryDialog = {
-        title: "Dietary preference",
-        message: "What's your dietary requirement?",
-        options: {
-          type: "radio",
-          model: "opt2",
-          items: [
-            { label: "Vegatarian", value: "vegetarian", color: "secondary" },
-            { label: "Vegan", value: "vegan" },
-            { label: "Dairy-free", value: "dairy-free" },
-            { label: "Regular", value: "regular" }
-          ]
-        }
-      };
-
-      let dialog = {
-        ...defaultDialogOpts,
-        ...(option === "luggage" ? luggageDialog : dietaryDialog)
-      };
-
-      this.$q
-        .dialog(dialog)
-        .then(choice => this.$q.notify(`${option}: ${choice}`))
-        .catch(() => "No option selected");
-    }
-  },
-  async mounted() {
-    /** Amplify clears out cookies and any storage that can map to users
-     * However it is on us to clear out our own store and redirect to Auth
-     * If customer decides to sign out we redirect it to home, and subsequentially to authentication
-     */
-    AmplifyEventBus.$on("authState", info => {
-      if (info === "signedOut") {
-        this.$store
-          .dispatch("profile/getSession")
-          .catch(
-            this.$router.push({ name: "auth", query: { redirectTo: "home" } })
-          );
-      }
-    });
-
-    // authentication guards prevent authenticated users to view Profile
-    // however, the component doesn't stop from rendering asynchronously
-    // this guarantees we attempt talking to Loyalty service
-    // if our authentication guards && profile module have an user in place
-    try {
-      if (this.isAuthenticated) {
-        await this.$store.dispatch("loyalty/fetchLoyalty");
-      }
-    } catch (error) {
-      console.error(error);
-      this.$q.notify(
-        `Error while fetching Loyalty - Check browser console messages`
-      );
-    }
-  }
-};
-</script>
-
-<style lang="stylus">
-/**
- * Amplify authenticatior HOC as of now doesn't provide overriding mechanisms for UI
- * we use CSS Root variables along with an authentication-form injected class for consistent experience
- */
-@import '~variables'
-
-a
-  text-decoration none
-  color black
-
-.loyalty__heading
-  &--tier
-    margin $between-content-margin
-
-.loyalty__progress
-  &--points > *
-    margin $between-content-margin
-
-  &--next-tier > *
-    margin $between-content-margin
-
-.profile__preferences-option
-  &:hover
-    cursor pointer
-
-:root
-  // Not a safe way to override as this can change at build
-  // https://github.com/aws-amplify/amplify-js/issues/2471
-  --amazonOrange $secondary !important
-  --color-primary $primary !important
-
-.authenticator__form
-  @media only screen and (min-device-width: 700px)
-    margin auto
-    padding 15vmin
-
-  > *
-    font-family 'Raleway', 'Open Sans', sans-serif
-
-  @media only screen and (min-device-width: 300px) and (max-device-width: 700px)
-    > div
-      min-width 80vw
-      padding 10vmin
-</style>
