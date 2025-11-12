2f59a17391941e8ef7bd1a11c16bd32ec9f5c2da
Kavya <146416207+Kavya-sm@users.noreply.github.com>
2025-11-12 22:50:35 +0530

    Update state.js

--- src/frontend/store/loyalty/state.js
+++ src/frontend/store/loyalty/state.js
@@ -1,9 +1,13 @@
 /**
- *
  * Loyalty [Vuex Module](https://vuex.vuejs.org/guide/modules.html) - It holds a customer loyalty information.
  * @see {@link SET_LOYALTY} for more info on mutation
  * @see {@link fetchLoyalty} for more info on action
  */
 export default {
-  loyalty: []
+  loyalty: {
+    points: 0,
+    level: "bronze",
+    remainingPoints: 0,
+    percentage: 0
+  }
 };
